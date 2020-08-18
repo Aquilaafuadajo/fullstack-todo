a18744af0b598831e49bf119704cca6435512caf
Emmanuel Afuadajo <aquilaafuadajo@gmail.com>
2020-08-18 03:30:36 +0100

    updated package.json

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
-
+const compression = require('compression');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController')
 const userRouter = require('./routes/userRoutes');
 
-const app = express()
+const app = express(compression())
 
 app.use(express.json());
+
+app.use()
 
 app.use('/api/v1/users', userRouter)
 
